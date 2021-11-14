164b46c1ed2011b145bc21e750aae6bc0bb2a22d
Ken Bruggeman <47519426+KenBruggeman@users.noreply.github.com>
2021-11-14 14:21:30 +0100

    Delete android-sdk-install.sh

--- scripts/android-sdk-install.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env bash
-cd /opt
-wget http://dl.google.com/android/android-sdk_r24.4.1-linux.tgz
-tar zxvf android-sdk_r24.4.1-linux.tgz
-rm android-sdk_r24.4.1-linux.tgz
-export ANDROID_HOME="/opt/android-sdk-linux"
-export PATH="$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:$PATH"
-android update sdk --no-ui
